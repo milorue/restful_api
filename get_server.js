e5a29980e73d2af9a04d197c21a8956f7686525b
mrue <milorue@gmail.com>
2020-05-05 17:06:21 -0400

    modified get process

--- get_server.js
+++ get_server.js
@@ -1,24 +1,5 @@
 const request = require('request')
 
-const http = require('http')
-
-const api = {
-    hostname: 'localhost',
-    port: 300,
-    path: '/boyz',
-    method: 'GET',
-}
-
-const req = http.request(api, res =>{
-    console.log('status Code: ' + res.statusCode)
-
-    res.on('data', d =>{
-        console.log(d)
-    })
+request('https://mrue-api.herokuapp.com/boyz', {json: true}, (req, res, body) => {
+    console.log(body)
 })
-
-req.on('error', err =>{
-    console.log('drat')
-    console.error(err)
-})
-req.end
